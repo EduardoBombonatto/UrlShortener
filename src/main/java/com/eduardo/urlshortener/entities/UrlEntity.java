b765e205824c9d987c4f2810a167b419c0b7139a
EduardoBombonatto <eduardobombonattolorenzetti@gmail.com>
2026-02-21 23:34:02 -0300

    first commit

---
+++ src/main/java/com/eduardo/urlshortener/entities/UrlEntity.java
@@ -0,0 +1,4 @@
+package com.eduardo.urlshortener.entities;
+
+public class UrlEntity {
+}
---
+++ src/test/java/com/eduardo/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
